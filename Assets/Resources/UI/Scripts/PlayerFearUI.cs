192b8378a25432356866821b9bd96470eda2226c
wenghoe <wenghoeng@gmail.com>
2019-09-02 16:52:50 +0900

    Move scripts into scripts folder. Change fear bar to a radial one. Change PlayerFearUI calcuation of the fear amount to update the fear bar.

--- Assets/Resources/UI/Scripts/PlayerFearUI.cs
+++ Assets/Resources/UI/Scripts/PlayerFearUI.cs
@@ -3,24 +3,24 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-[RequireComponent(typeof(RawImage))]
+[RequireComponent(typeof(Image))]
 public class PlayerFearUI : MonoBehaviour
 {
 
-    RawImage fearRawImage;
+    Image fearImage;
     Player player;
 
     // Use this for initialization
     void Start()
     {
         player = FindObjectOfType<Player>();
-        fearRawImage = GetComponent<RawImage>();
+        fearImage = GetComponent<Image>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        float yValue = -(player.fearAsPercentage / 2f) - 0.5f;
-        fearRawImage.uvRect = new Rect(0f, yValue, 1f, 0.5f); // vertical bar like in DL
+        float amount = player.fearAsPercentage;
+        fearImage.fillAmount = amount;
     }
 }
